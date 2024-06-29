0012e7ecebca747777f4fb7e4ad8d0bb4e2e29e0
Matt <contractdeveloper0@gmail.com>
2024-06-29 14:23:44 +0100

    Added REST APIs and tested all

--- django_react_app/books/views.py
+++ django_react_app/books/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+
+from .models import Book
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def books_list(request):
+    if request.method == 'GET':
+        data = Book.objects.all()
+        serializer = BookSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['PUT', 'DELETE'])
+def books_detail(request, id):
+    try:
+        book = Book.objects.get(id=id)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- django_react_app/django_react_app/urls.py
+++ django_react_app/django_react_app/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from books import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/books/$', views.books_list),
+    re_path(r'^api/books/([0-9])$', views.books_detail),
 ]
